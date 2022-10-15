e5526731596e1121baead3e62dcc081daa5a1a51
peter6053 <peternjuguna76@gmail.com>
2022-10-15 16:23:08 +0300

    customizing the notification

--- app/src/main/java/com/example/pushinglocalnotifications/NotificationChannel/CounterNotificationService.kt
+++ app/src/main/java/com/example/pushinglocalnotifications/NotificationChannel/CounterNotificationService.kt
@@ -1,6 +1,19 @@
 package com.example.pushinglocalnotifications.NotificationChannel
 
-class CounterNotificationService {
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import com.example.pushinglocalnotifications.R
+
+class CounterNotificationService(private val context: Context) {
+
+    fun shownotification(Counter: Int) {
+
+        val notification = NotificationCompat.Builder(context, Counter_Channel_id)
+            .setSmallIcon(R.drawable.ic_baseline_baby_changing_station_24)
+
+
+    }
+
     companion object {
         const val Counter_Channel_id = "counterchannel"
     }
